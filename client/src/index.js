fd0306f8513d57bb6744be4b4f0c3fc88afa702a
sezal <sezaldhiman701@gmail.com>
2026-06-04 18:17:13 +0530

    changes

--- client/src/index.js
+++ client/src/index.js
@@ -7,15 +7,15 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+ <GoogleOAuthProvider
+  clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+>
   <App />
 </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
 
 
